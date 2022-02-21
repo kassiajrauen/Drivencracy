c2b7394ffff8cce14759ad6567faed7eaaeb4bd2
KassiaJRauen <kassia.grj@gmail.com>
2022-02-21 14:24:03 -0300

    add feat/vote

---
+++ src/controllers/voteController.js
@@ -0,0 +1,22 @@
+import db from '../db.js';
+import { ObjectId } from 'mongodb';
+
+export async function vote(req, res) {
+    console.log(req.params.id, "aqui")
+    const idChoice = new ObjectId(req.params.id);
+
+    try {
+        const choiceExist = await db.collection('choices').findOne({_id: idChoice});
+        if(!choiceExist) {
+            return res.sendStatus(404);
+        }
+
+        const insertVote = await db.collection('votes').insertOne({vote: idChoice, date: Date.now(), poolId: choiceExist.poolId});
+        if(insertVote){
+            res.sendStatus(201);
+        }
+
+    } catch (error) {
+        res.sendStatus(500);
+    }
+}
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express, {json} from 'express';
 import cors from 'cors';
 import {createPool, getPool} from './controllers/poolController.js';
 import {createChoice, getChoice} from './controllers/choiceController.js';
+import {vote} from './controllers/voteController.js';
 
 const server = express();
 server.use(json());
@@ -13,7 +14,9 @@ server.get('/pool', getPool);
 
 server.post('/choice', createChoice);
 
-server.get('/choice', getChoice);
+server.get('/pool/:id/choice', getChoice);
+
+server.post('/choice/:id/vote', vote);
 
 server.listen(5000, () => {
     console.log("Listening on 5000")
